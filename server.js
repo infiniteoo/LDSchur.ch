ff763e96fe1abe17f56a11be7d272b5e485eb90d
Troy Dorman <troydorman@gmail.com>
2021-08-10 17:31:22 -0600

    url shortening service appears to be working

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 
-const app = express()
+const app = express();
+connectDB();
 
-connectDB()
+app.use(express.json({ extended: false }));
 
 // define routes
-app.use('/', require('./routes/index'))
-app.use('/api/url', require('./routes/url'))
+app.use("/", require("./routes/index"));
+app.use("/api/url", require("./routes/url"));
 
-const PORT = process.env.PORT || 7777
+const PORT = process.env.PORT || 7777;
+
+app.get("/api", (req, res) => {
+  res.json({ message: "hello, you've reached the api route." });
+});
 
 app.listen(PORT, () => console.log(`🚀 Server Live 🌎 @ port ${PORT}`))
 
