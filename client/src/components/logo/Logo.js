53606e62f005885eea3c754e4e52c8d998b8b991
Troy Dorman <troydorman@gmail.com>
2021-08-16 15:41:05 -0600

    styled our logo and centered it in a grid

--- client/src/components/logo/Logo.js
+++ client/src/components/logo/Logo.js
@@ -1,13 +1,39 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
 import LDSLogo from "../../imgs/lds.png";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    borderRadius: "50%",
+  },
+});
 
 const Logo = () => {
+  const classes = useStyles();
   return (
     <div>
-      <Paper variant="outlined">
-        <img src={LDSLogo} alt="Minimal LDS Logo" />
-      </Paper>
+      <Grid
+        container
+        alignItems="center"
+        alignContent="center"
+        justify="center"
+        lg="12"
+        md="12"
+        sm="12"
+        xs="12"
+      >
+        <Grid item>
+          <Paper variant="outlined">
+            <img
+              src={LDSLogo}
+              alt="Minimal LDS Logo"
+              className={classes.root}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
