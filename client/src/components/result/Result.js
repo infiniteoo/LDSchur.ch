c476564ecaa201c5e02f7c13884c66f2b8481c40
Troy Dorman <troydorman@gmail.com>
2021-09-28 22:40:39 -0600

    displaying returned link from database

--- client/src/components/result/Result.js
+++ client/src/components/result/Result.js
@@ -1,14 +1,19 @@
-import React from "react";
+import { useEffect } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import CardActions from "@material-ui/core/CardActions";
-import Box from "@material-ui/core/Box";
 import { Grid } from "@material-ui/core";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
-const Result = () => {
+const Result = (props) => {
+  useEffect(() => {
+    console.log("helllo", props.shortURL);
+    return () => {
+      console.log(props.shortURL);
+    };
+  }, [props.shortURL]);
   return (
     <Grid container justifyContent="center">
       <Grid item /*  xl={2} lg={2} md={2} sm={4} xs={6} */>
@@ -19,7 +24,7 @@ const Result = () => {
               variant="h4"
               className="resultsBox"
             >
-              https://LDSchur.ch/sx$23f
+              {props.shortURL}
             </Typography>
           </CardContent>
           <CardActions>
--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -29,7 +29,7 @@ const Search = () => {
           setUrlToConvert={setUrlToConvert}
         />
 
-        <Result />
+        <Result shortURL={shortURL} />
       </Container>
     </div>
   );
