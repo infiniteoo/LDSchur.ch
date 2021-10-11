fde673f766247868476ab756b02feb03ff1a5d7f
Troy Dorman <troydorman@gmail.com>
2021-10-11 09:06:42 -0600

    updated the FAQ questions and answeres

--- client/src/components/middleSplash/FAQ.js
+++ client/src/components/middleSplash/FAQ.js
@@ -42,8 +42,10 @@ function FAQ() {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            A URL shortener, also known as a link shortener, works by
+            transforming any long URL into a shorter, more readable link. When a
+            user clicks the shortened version, they’re automatically forwarded
+            to the original, longer URL.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -59,8 +61,11 @@ function FAQ() {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Extremely long website URLs can be confusing and intimidating to
+            users. Plus, on social media platforms such as Twitter, you are
+            limited to the amount of characters you can use. A short URL will
+            condense a really long website address into a few characters. This
+            will make it easier to share the link and overall increase traffic.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -76,8 +81,7 @@ function FAQ() {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Other URL shortening services allow you to convert ANY long link into a short link.  This means they could send you to a fake link, or a link unsuitable for a wholesome person's eyes, such as pornography or violent content.  SafeLink&#174; Technology is a built-in custom filter only found on on our platform, that removes the ability to do this, and ensures when a user receives an LDSchur.ch link, they are guaranteed to be safe. 
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -94,8 +98,7 @@ function FAQ() {
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            While the creators of this website are active, temple-recommended members of The Church of Jesus Christ of Latter-Day Saints, we are not directly affiliated, endorsed or run by the church.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -106,13 +109,12 @@ function FAQ() {
           id="panel2a-header"
         >
           <Typography className={classes.accordion}>
-            Why Choose LDSChur.ch?
+            Why Did You Build This Website?
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            We built this website to serve our Lord Jesus Christ and to help people find the best short links for their websites.  We hope you enjoy using it, and we hope you will join us in our mission to spread the Gospel of Jesus Christ.  We also hope you will share your own LDSchur.ch links with your friends and family.  In addition, this site was built as a demostration of our development skills to showcase our ability on our portfolio, and one day to find full-time employment as a full-stack web developer.
           </Typography>
         </AccordionDetails>
       </Accordion>
