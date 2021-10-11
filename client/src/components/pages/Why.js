966c83d5db5ea422de4dec44bbe92afc3307db24
Troy Dorman <troydorman@gmail.com>
2021-10-11 14:38:52 -0600

    finished why template page.  added imgs.

--- client/src/components/pages/Why.js
+++ client/src/components/pages/Why.js
@@ -5,6 +5,10 @@ import { Grid } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import jesusPortrait from "../../imgs/jesus_portrait.jpg";
 import Footer from "../footer/Footer";
+import classroom from "../../imgs/classroom.jpg";
+import programming from "../../imgs/programming.jpg";
+import pricing from "../../imgs/pricing.jpg";
+import enterprise from "../../imgs/enterprise.jpg";
 
 const useStyles = makeStyles({
   jumbotron: {
@@ -26,10 +30,11 @@ const useStyles = makeStyles({
   headline: {
     fontFamily: ["Georama"],
     fontWeight: "100",
-    color: "black",
+    color: "white",
+    textShadow: "2px 2px 4px #000000",
     backgroundColor: "#E7D777",
     textAlign: "center",
-    background: "rgb(255,255,255)",
+
     marginLeft: "60px",
     background:
       "linear-gradient(120deg, rgba(255,255,255,1) 11%, rgba(231,215,119,1) 64%, rgba(255,255,255,1) 88%)",
@@ -69,71 +74,109 @@ export default function Why() {
       </div>
 
       <Typography className={classes.subheadline} variant="h3" component="h3">
-        URL Shortening 101
+        <strong>URL Shortening 101</strong>
       </Typography>
+
       <div className={classes.infoContainer}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab harum
-        itaque nisi eligendi omnis pariatur qui nobis, tenetur sit magnam
-        aliquam quaerat beatae iste libero sequi repudiandae porro voluptas
-        architecto commodi dicta debitis! Reiciendis voluptatum provident ut
-        reprehenderit est quas harum doloribus ullam explicabo ex! Explicabo
-        provident natus omnis atque sunt architecto minima veritatis nam
-        expedita officiis voluptatum fugiat eos saepe, assumenda excepturi fugit
-        molestias? Fuga accusantium harum doloremque eligendi vitae! Porro nemo,
-        atque aspernatur ea esse, cum placeat enim dolor numquam beatae
-        accusamus rem fugit aut delectus explicabo quidem, saepe optio
-        praesentium adipisci repellat amet magnam. Veritatis, ipsa?
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={7} sm={7} md={7} lg={7}>
+            {" "}
+            <img src={classroom} alt="" style={{ borderRadius: "10px" }} />
+          </Grid>
+          <Grid item xs={5} sm={5} md={5} lg={5}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab
+            harum itaque nisi eligendi omnis pariatur qui nobis, tenetur sit
+            magnam aliquam quaerat beatae iste libero sequi repudiandae porro
+            voluptas architecto commodi dicta debitis! Reiciendis voluptatum
+            provident ut reprehenderit est quas harum doloribus ullam explicabo
+            ex! Explicabo provident natus omnis atque sunt architecto minima
+            veritatis nam expedita officiis voluptatum fugiat eos saepe,
+            assumenda excepturi fugit molestias? Fuga accusantium harum
+            doloremque eligendi vitae! Porro nemo, atque aspernatur ea esse, cum
+            placeat enim dolor numquam beatae accusamus rem fugit aut delectus
+            explicabo quidem, saepe optio praesentium adipisci repellat amet
+            magnam. Veritatis, ipsa?
+          </Grid>
+        </Grid>
       </div>
 
       <Typography className={classes.subheadline} variant="h3" component="h3">
-        Integration & API
+        <strong>Integration & API</strong>
       </Typography>
+
       <div className={classes.infoContainer}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab harum
-        itaque nisi eligendi omnis pariatur qui nobis, tenetur sit magnam
-        aliquam quaerat beatae iste libero sequi repudiandae porro voluptas
-        architecto commodi dicta debitis! Reiciendis voluptatum provident ut
-        reprehenderit est quas harum doloribus ullam explicabo ex! Explicabo
-        provident natus omnis atque sunt architecto minima veritatis nam
-        expedita officiis voluptatum fugiat eos saepe, assumenda excepturi fugit
-        molestias? Fuga accusantium harum doloremque eligendi vitae! Porro nemo,
-        atque aspernatur ea esse, cum placeat enim dolor numquam beatae
-        accusamus rem fugit aut delectus explicabo quidem, saepe optio
-        praesentium adipisci repellat amet magnam. Veritatis, ipsa?
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={7} sm={7} md={7} lg={7}>
+            {" "}
+            <img src={programming} alt="" style={{ borderRadius: "10px" }} />
+          </Grid>
+          <Grid item xs={5} sm={5} md={5} lg={5}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab
+            harum itaque nisi eligendi omnis pariatur qui nobis, tenetur sit
+            magnam aliquam quaerat beatae iste libero sequi repudiandae porro
+            voluptas architecto commodi dicta debitis! Reiciendis voluptatum
+            provident ut reprehenderit est quas harum doloribus ullam explicabo
+            ex! Explicabo provident natus omnis atque sunt architecto minima
+            veritatis nam expedita officiis voluptatum fugiat eos saepe,
+            assumenda excepturi fugit molestias? Fuga accusantium harum
+            doloremque eligendi vitae! Porro nemo, atque aspernatur ea esse, cum
+            placeat enim dolor numquam beatae accusamus rem fugit aut delectus
+            explicabo quidem, saepe optio praesentium adipisci repellat amet
+            magnam. Veritatis, ipsa?
+          </Grid>
+        </Grid>
       </div>
 
       <Typography className={classes.subheadline} variant="h3" component="h3">
-        Enterprise Class
+        <strong>Enterprise Class</strong>
       </Typography>
       <div className={classes.infoContainer}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab harum
-        itaque nisi eligendi omnis pariatur qui nobis, tenetur sit magnam
-        aliquam quaerat beatae iste libero sequi repudiandae porro voluptas
-        architecto commodi dicta debitis! Reiciendis voluptatum provident ut
-        reprehenderit est quas harum doloribus ullam explicabo ex! Explicabo
-        provident natus omnis atque sunt architecto minima veritatis nam
-        expedita officiis voluptatum fugiat eos saepe, assumenda excepturi fugit
-        molestias? Fuga accusantium harum doloremque eligendi vitae! Porro nemo,
-        atque aspernatur ea esse, cum placeat enim dolor numquam beatae
-        accusamus rem fugit aut delectus explicabo quidem, saepe optio
-        praesentium adipisci repellat amet magnam. Veritatis, ipsa?
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={7} sm={7} md={7} lg={7}>
+            {" "}
+            <img src={enterprise} alt="" style={{ borderRadius: "10px" }} />
+          </Grid>
+          <Grid item xs={5} sm={5} md={5} lg={5}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab
+            harum itaque nisi eligendi omnis pariatur qui nobis, tenetur sit
+            magnam aliquam quaerat beatae iste libero sequi repudiandae porro
+            voluptas architecto commodi dicta debitis! Reiciendis voluptatum
+            provident ut reprehenderit est quas harum doloribus ullam explicabo
+            ex! Explicabo provident natus omnis atque sunt architecto minima
+            veritatis nam expedita officiis voluptatum fugiat eos saepe,
+            assumenda excepturi fugit molestias? Fuga accusantium harum
+            doloremque eligendi vitae! Porro nemo, atque aspernatur ea esse, cum
+            placeat enim dolor numquam beatae accusamus rem fugit aut delectus
+            explicabo quidem, saepe optio praesentium adipisci repellat amet
+            magnam. Veritatis, ipsa?
+          </Grid>
+        </Grid>
       </div>
 
       <Typography className={classes.subheadline} variant="h3" component="h3">
-        Pricing
+        <strong>Pricing</strong>
       </Typography>
       <div className={classes.infoContainer}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab harum
-        itaque nisi eligendi omnis pariatur qui nobis, tenetur sit magnam
-        aliquam quaerat beatae iste libero sequi repudiandae porro voluptas
-        architecto commodi dicta debitis! Reiciendis voluptatum provident ut
-        reprehenderit est quas harum doloribus ullam explicabo ex! Explicabo
-        provident natus omnis atque sunt architecto minima veritatis nam
-        expedita officiis voluptatum fugiat eos saepe, assumenda excepturi fugit
-        molestias? Fuga accusantium harum doloremque eligendi vitae! Porro nemo,
-        atque aspernatur ea esse, cum placeat enim dolor numquam beatae
-        accusamus rem fugit aut delectus explicabo quidem, saepe optio
-        praesentium adipisci repellat amet magnam. Veritatis, ipsa?
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={7} sm={7} md={7} lg={7}>
+            {" "}
+            <img src={pricing} alt="" style={{ borderRadius: "10px" }} />
+          </Grid>
+          <Grid item xs={5} sm={5} md={5} lg={5}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab
+            harum itaque nisi eligendi omnis pariatur qui nobis, tenetur sit
+            magnam aliquam quaerat beatae iste libero sequi repudiandae porro
+            voluptas architecto commodi dicta debitis! Reiciendis voluptatum
+            provident ut reprehenderit est quas harum doloribus ullam explicabo
+            ex! Explicabo provident natus omnis atque sunt architecto minima
+            veritatis nam expedita officiis voluptatum fugiat eos saepe,
+            assumenda excepturi fugit molestias? Fuga accusantium harum
+            doloremque eligendi vitae! Porro nemo, atque aspernatur ea esse, cum
+            placeat enim dolor numquam beatae accusamus rem fugit aut delectus
+            explicabo quidem, saepe optio praesentium adipisci repellat amet
+            magnam. Veritatis, ipsa?
+          </Grid>
+        </Grid>
       </div>
 
       <Footer />
