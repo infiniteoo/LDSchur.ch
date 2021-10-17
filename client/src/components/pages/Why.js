db1fe8a514e3fe75426ffb7dff2ab106f989a65a
Troy Dorman <troydorman@gmail.com>
2021-10-17 00:39:08 -0600

    finished why design.  can now use this as template for other pages.  grats!

--- client/src/components/pages/Why.js
+++ client/src/components/pages/Why.js
@@ -1,8 +1,7 @@
 import React, { useLayoutEffect } from "react";
 import { makeStyles } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
-import { Grid } from "@material-ui/core";
-import { Typography } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 import jesusPortrait from "../../imgs/jesus_portrait.jpg";
 import Footer from "../footer/Footer";
 import classroom from "../../imgs/education.svg";
@@ -11,61 +10,29 @@ import pricing from "../../imgs/pricing.svg";
 import enterprise from "../../imgs/security.svg";
 import "./why.css";
 
-const useStyles = makeStyles({
-  jumbotron: {
-    marginTop: "6rem",
-    display: "flex",
-    marginBottom: "50px",
-  },
-  "@media (max-width: 780px)": {
-    jumbotron: {
-      display: "block",
-    },
-    img: {
-      width: "90%",
-      marginBottom: "20px",
-    },
-    headline: {
-      fontSize: "2.5rem",
-    },
-  },
-
-  headline: {
-    fontFamily: ["Georama"],
-    fontWeight: "100",
-    color: "white",
-    textShadow: "2px 2px 4px #000000",
-    backgroundColor: "#E7D777",
-    textAlign: "center",
-
-    marginLeft: "60px",
-    background:
-      "linear-gradient(120deg, rgba(255,255,255,1) 11%, rgba(231,215,119,1) 64%, rgba(255,255,255,1) 88%)",
-  },
-});
-
 export default function Why() {
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   });
-  const classes = useStyles();
+
   return (
     <Container>
-      <div className={classes.jumbotron}>
-        <img src={jesusPortrait} className="titlepic" alt="" />
-        <Grid container justifyContent="center" alignItems="center">
-          <Grid item xs={12} sm={12} md={12} lg={12}>
-            <Typography
-              className={classes.headline}
-              variant="h1"
-              component="h2"
-            >
-              Why?
-            </Typography>
-          </Grid>
-        </Grid>
-      </div>
       <div className="container">
+        <div className="jumbotron">
+          <img
+            src={jesusPortrait}
+            className="titlepic"
+            alt="portrait of jesus"
+          />
+          <Typography
+            className="headline"
+            variant="h1"
+            component="h2"
+            style={{ fontFamily: ["Georama", "sans-serif"] }}
+          >
+            Why?
+          </Typography>
+        </div>
         <section className="card">
           <img src={classroom} alt="" />
           <div>
@@ -74,11 +41,8 @@ export default function Why() {
             </h3>
             <p>
               Most URL links are long, confusing and cumbersome. As a result,
-              people are reluctant to click them. Plus, have you tried sharing a
-              long Facebook link on Twitter? You're lucky if you have enough
-              characters to post it! That's where a URL shortner comes in. We'll
-              take that ginormous link and make it into a teeny-tiny link that's
-              easy to share.
+              people are reluctant to click them. We'll take that ginormous link
+              and make it into a teeny-tiny link that's easy to share.
             </p>
           </div>
         </section>
@@ -102,12 +66,11 @@ export default function Why() {
               <strong>Safe Link Technology</strong>
             </h3>
             <p>
-              Other URL shorteners are not safe. They will literally let you
-              share any link from any website. This means someone could send you
-              pornography, or blasphemous material, or violent content without
-              you even knowing it. Not with us! Our SafeLink integrated filter
-              technology prevents people from sharing links from nefarious
-              websites.
+              Other URL shorteners are not safe. They will let you share any
+              link from anywhere. This means someone could send you harmful
+              content without you even knowing it. Not with us! Our SafeLink
+              integrated filter technology prevents people from sharing
+              nefarious links.
             </p>
           </div>
         </section>
